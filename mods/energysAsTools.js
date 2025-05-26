2a1cc8e5374feaf9cda06f34e8ebc0a1d8c69844
Aitorbrine <140336973+Aitorbrine@users.noreply.github.com>
2025-05-26 08:51:08 +0200

    Create energysAsTools.js

---
+++ mods/energysAsTools.js
@@ -0,0 +1,8 @@
+elements.room_temp.category = "tools"; 
+elements.incinerate.category = "tools";
+elements.cook.category = "tools"; 
+elements.heat_ray.category = "tools"; 
+elements.freeze_ray.category = "tools"; 
+elements.unburn.category = "tools"; 
+elements.unpaint.category = "tools"; 
+elements.uncharge.category = "tools"; 
